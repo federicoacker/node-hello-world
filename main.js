9c4fa84ee54858e2eea28eccab025865815494c8
federicoacker <acker.federico@gmail.com>
2026-05-20 14:10:56 +0200

    aggiunto la parte più importante di tutte che era il cowsay

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-
+import { cowsay } from "cowsayjs";
 import askClaude from "./functions/askClaude.js";
 import options from "./functions/getOptions.js";
 
 askClaude(options)
-.then(response => console.log(response))
+.then(response => console.log(cowsay(response)))
 .catch(error => console.error(error));
