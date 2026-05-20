e7e67872f86f505f784a12d42a9ae58cbf552624
federicoacker <acker.federico@gmail.com>
2026-05-20 13:50:53 +0200

    Creato un sistema molto rudimentario per prendermi gli arguments e segnarmeli per fare poi la chiamata dopo ad antropic

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 
-const arrayDiArgomenti = process.argv.splice(0, 2);
+const argumentArray = process.argv.splice(2);
 
-for(let i = 0; i < arrayDiArgomenti.length; i++){
-    console.log(arrayDiArgomenti[i]);
+const options = {
+    command: "",
+    category: "",
 }
+
+for(let i = 0; i < argumentArray.length; i++){
+    const current = argumentArray[i];
+    console.log(current);
+    if(current.startsWith("--") && !current.includes("=")){
+        options.command=current;
+    }
+    if(current.startsWith("--category=")){
+        options.category=current.split("=")[1];
+    }
+}
+
